dc61970a689ae3a646713f04eded0db8379a76d4
Kirill <101171995+KirillVM@users.noreply.github.com>
2023-12-05 08:36:16 +0000

    chore: add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+.(js|jsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '^@src/(.*)$': '<rootDir>src/$1',
+    '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^@components/(.*)$': '<rootDir>src/components/$1',
+    '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@sass/(.*)$': '<rootDir>/src/sass/$1',
+    '^@store/(.*)$': '<rootDir>/src/store/$1',
+    '^@types/(.*)$': '<rootDir>/src/types/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
+  // setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+};
