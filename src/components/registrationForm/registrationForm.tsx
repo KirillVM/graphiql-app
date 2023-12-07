3c06699c1a1259ce4900bbcf7d6047a5328efb39
Kirill <101171995+KirillVM@users.noreply.github.com>
2023-12-07 09:21:56 +0000

    feat: add yupResolver to registration form, add button, error and input styles to registration form

--- src/components/registrationForm/registrationForm.tsx
+++ src/components/registrationForm/registrationForm.tsx
@@ -1,60 +1,85 @@
+import './registrationForm.scss';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { FormData } from './registrationForm.interfaces';
-import './registrationForm.scss';
+import registrationFormSchema from './registrationForm.schema';
+import { yupResolver } from '@hookform/resolvers/yup';
+// import { useNavigate } from 'react-router-dom';
 
 const RegistrationForm = (): JSX.Element => {
-  const methods = useForm<FormData>();
-  const handleSubmit: SubmitHandler<FormData> = (data) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(registrationFormSchema),
+    mode: 'onChange',
+  });
+  const { fname, email, password, confirmPassword } = errors;
+  // dispatch = useAppDispatch();
+  // const navigate = useNavigate();
+  const onSubmitHandler: SubmitHandler<FormData> = (data) => {
     console.log(data);
+    // dispatch(setFromData(data))
+    // navigate(to: '/main');
   };
   return (
     <>
-      <form className="form" onSubmit={methods.handleSubmit(handleSubmit)}>
+      <form className="form" onSubmit={handleSubmit(onSubmitHandler)}>
+        <h1>Registration</h1>
         <label htmlFor="fname" className="form__row">
           Name
           <input
-            {...methods.register('fname', {
+            {...register('fname', {
               required: true,
             })}
             id="fname"
             className="form__fname"
-            placeholder="Name"
+            placeholder="Ivan"
           ></input>
+          {fname ? <p>{fname.message}</p> : <p> </p>}
         </label>
         <label htmlFor="email" className="form__row">
           Email
           <input
-            {...methods.register('email', {
+            {...register('email', {
               required: true,
             })}
             id="email"
             className="form__email"
-            placeholder="Email"
+            placeholder="email@gmail.com"
           ></input>
+          {email ? <p>{email.message}</p> : <p> </p>}
         </label>
         <label htmlFor="password" className="form__row">
           Password
           <input
-            {...methods.register('password', {
+            {...register('password', {
               required: true,
             })}
             id="password"
             className="form__password"
             placeholder="Password"
           ></input>
+          {password ? <p>{password.message}</p> : <p> </p>}
         </label>
         <label htmlFor="confirm-password" className="form__row">
           Confirm password
           <input
-            {...methods.register('confirmPassword', {
+            {...register('confirmPassword', {
               required: true,
             })}
             id="confirm-password"
             className="form__confirmPassword"
             placeholder="Confirm password"
           ></input>
+          {confirmPassword ? <p>{confirmPassword.message}</p> : <p> </p>}
         </label>
-        <button className="form__button">Submit</button>
+        <button
+          className="form__button"
+          disabled={Object.keys(errors).length > 0}
+        >
+          Submit
+        </button>
       </form>
     </>
   );
