c05bec219ea940d06940098a6bb4f59c4061dfeb
climbingirl <48901488+climbingirl@users.noreply.github.com>
2023-12-09 20:50:00 +0500

    fix: fix global styles import

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router/router';
-import '@styles/global.scss';
+import 'src/styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <RouterProvider router={router} />
