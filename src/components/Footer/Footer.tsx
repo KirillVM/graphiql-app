f84c6000a3724156f36b75dd246b7be16105bc34
Liza <isupova.97@mail.ru>
2023-12-10 16:31:57 +0300

    feat: add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,32 @@
+import rss_logo from '../../assets/icons/rss-logo.svg';
+import github_logo from '../../assets/icons/github.svg';
+import styles from './footer.module.scss';
+
 const Footer = () => {
-  return <div className="footer"></div>;
+  const githubUsers = ['climbingirl', 'kotangenss', 'KirillVM'];
+
+  return (
+    <footer>
+      <div className={styles.container}>
+        <a target="_blank" href="https://rs.school/react/" rel="noreferrer">
+          <img src={rss_logo} alt="Rolling Scopes School logo" />
+        </a>
+        <p className={styles.year}>2023</p>
+        <div className={styles.githubs}>
+          {githubUsers.map((developer) => (
+            <a
+              key={developer}
+              target="_blank"
+              href={`https://github.com/${developer}`}
+              rel="noreferrer"
+            >
+              <img src={github_logo} alt="Github icon" />
+            </a>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
