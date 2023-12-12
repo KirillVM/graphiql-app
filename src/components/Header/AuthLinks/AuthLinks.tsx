a6a5f0e31802db9273c548fbbe1b3221e73c8d95
climbingirl <isvetlanaivanova@yandex.ru>
2023-12-12 22:18:49 +0700

    refactor: refactor AuthLinks component

---
+++ src/components/Header/AuthLinks/AuthLinks.tsx
@@ -0,0 +1,33 @@
+import { NavLink } from 'react-router-dom';
+import { AuthLinkProps } from './AuthLinks.interface';
+import CustomButton from '../../CustomButton/CustomButton';
+import ROUTES from '../../../router/routes';
+import styles from './AuthLinks.module.scss';
+
+const AuthLink = ({ children, to, type }: AuthLinkProps) => {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) => (isActive ? styles.active : '')}
+    >
+      <CustomButton type={type} size="medium">
+        {children}
+      </CustomButton>
+    </NavLink>
+  );
+};
+
+const AuthLinks = () => {
+  return (
+    <div className={styles.btns_auth}>
+      <AuthLink to={ROUTES.SIGNIN} type="white">
+        Sign In
+      </AuthLink>
+      <AuthLink to={ROUTES.SIGNUP} type="black">
+        Sign Up
+      </AuthLink>
+    </div>
+  );
+};
+
+export default AuthLinks;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,17 @@
 import styles from './Header.module.scss';
-import ROUTES from '../../router/routes';
-import AuthLink from './AuthLink/AuthLink';
 import { useAuth } from '../../hooks/useAuth';
 import SignOutBtn from './SignOutBtn/SignOutBtn';
 import ButtonsLang from './ButtonsLang/ButtonLang';
+import AuthLinks from './AuthLinks/AuthLinks';
 
 const Header = () => {
   const { userToken } = useAuth();
-
-  const btnAuthContent = !userToken ? (
-    <>
-      <AuthLink to={ROUTES.SIGNIN} type="white">
-        Sign In
-      </AuthLink>
-      <AuthLink to={ROUTES.SIGNUP} type="black">
-        Sign Up
-      </AuthLink>
-    </>
-  ) : (
-    <SignOutBtn />
-  );
 
   return (
     <header>
       <div className={styles.container}>
         <ButtonsLang />
-        <div className={styles.btns_auth}>{btnAuthContent}</div>
+        {!userToken ? <AuthLinks /> : <SignOutBtn />}
       </div>
     </header>
   );
