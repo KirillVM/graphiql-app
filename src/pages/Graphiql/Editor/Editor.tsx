836058199bd451c55c3ba3abd3971d316b1fedaf
Liza <isupova.97@mail.ru>
2024-01-02 13:14:22 +0300

    feat: add prettifying support

--- src/pages/Graphiql/Editor/Editor.tsx
+++ src/pages/Graphiql/Editor/Editor.tsx
@@ -5,6 +5,7 @@ import { useAppDispatch, useAppSelector } from '../../../store/hooks';
 import { setEditorValue } from '../../../store/playgroundSlice/playgroundSlice';
 import { editorValueSelector } from '../../../store/playgroundSlice/playgroundSelectors';
 import { getGraphiqlData } from '../../../store/playgroundSlice/playgroundThunks';
+import prettifyIcon from '@assets/icons/prettify-query.svg';
 import styles from './Editor.module.scss';
 
 const Editor = () => {
@@ -17,6 +18,34 @@ const Editor = () => {
 
   const handleRequest = () => {
     dispatch(getGraphiqlData(null));
+  };
+
+  const handleRequestPrettify = () => {
+    const formattedValue = value
+      .replace(/}/g, '}\n')
+      .replace(/{/g, '{\n')
+      .replace(/}/g, '\n}');
+
+    const lines = formattedValue.split('\n');
+    let formattedQuery = '';
+    let level = 0;
+
+    lines.forEach((line) => {
+      const trimmedLine = line.trim();
+      const decreaseLevel = trimmedLine.startsWith('}') ? -1 : 0;
+      const increaseLevel = trimmedLine.endsWith('{') ? 1 : 0;
+
+      if (trimmedLine !== '') {
+        level += decreaseLevel;
+
+        formattedQuery +=
+          ' '.repeat(Math.max(0, level * 2)) + trimmedLine + '\n';
+
+        level += increaseLevel;
+      }
+    });
+
+    dispatch(setEditorValue(formattedQuery));
   };
 
   return (
@@ -33,6 +62,9 @@ const Editor = () => {
       </div>
       <div className={styles.tooll_bar}>
         <button className={styles.request_btn} onClick={handleRequest}></button>
+        <button className={styles.prettify_btn} onClick={handleRequestPrettify}>
+          <img src={prettifyIcon} alt="Prettyfy icon" />
+        </button>
       </div>
     </div>
   );
