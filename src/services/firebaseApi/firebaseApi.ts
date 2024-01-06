8d9773b91a324ffdfad34ef3a277d015655394de
KirillVM <forjustice55@gmail.com>
2024-01-06 21:50:36 +0300

    feat: update errorBoundaryFallback styles, fix firebaseApi functionality, update SignOutButton handleClick functionality

--- src/services/firebaseApi/firebaseApi.ts
+++ src/services/firebaseApi/firebaseApi.ts
@@ -1,83 +1,15 @@
 import { firebaseConfig } from './firebaseConfig';
 import { initializeApp } from 'firebase/app';
-import {
-  createUserWithEmailAndPassword,
-  getAuth,
-  signInWithCustomToken,
-  signInWithEmailAndPassword,
-  signOut,
-} from 'firebase/auth';
+import { getAuth, signOut } from 'firebase/auth';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
-// Sign up
-export const createUser = async (
-  email: string,
-  password: string
-): Promise<void> => {
-  return createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      console.log(user);
-      signInUser(email, password);
-    })
-    .catch((error) => {
-      const errorMessage = error.message;
-      switch (errorMessage) {
-        case 'EMAIL-EXIST':
-          console.error('User with this email already exist');
-      }
-    });
-};
-
-// Sign in
-export const signInUser = async (
-  email: string,
-  password: string
-): Promise<void> => {
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      userCredential.user.getIdToken().then((refreshToken) => {
-        localStorage.setItem('refreshToken', refreshToken);
-      });
-    })
-    .catch((error) => {
-      const errorMessage = error.message;
-      console.error(errorMessage);
-      switch (errorMessage) {
-        case 'INVALID_LOGIN_CREDENTIALS':
-          console.error(`User with these credentials doesn't exist`);
-      }
-    });
-};
-
-// Sign in with token
-export const signInWithToken = async (token: string): Promise<void> => {
-  signInWithCustomToken(auth, token)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      console.log(user);
-    })
-    .catch((error) => {
-      const errorMessage = error.message;
-      switch (errorMessage) {
-        case 'INVALID_CUSTOM_TOKEN':
-          console.error('The supplied token is not a Firebase custom token.');
-      }
-    });
-};
-
 // Sign out with token
 export const signOutUser = async (): Promise<void> => {
-  signOut(auth)
-    .then(() => {
-      localStorage.removeItem('refreshToken');
-      console.log('User succesfuly signed out');
-    })
-    .catch((error) => {
-      const errorMessage = error.message;
-      console.error(errorMessage);
-    });
+  signOut(auth).catch((error) => {
+    const errorMessage = error.message;
+    console.error(errorMessage);
+  });
 };
